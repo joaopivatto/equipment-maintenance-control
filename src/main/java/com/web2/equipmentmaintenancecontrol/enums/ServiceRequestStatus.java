e3a8d156be76848ba3a41c9e8a76e536d6e288a3
˜Samuel <samuel.natel@ufpr.br>
2026-08-18 13:24:24 -0300

    feat: adicionada classe enum com status de serviços

---
+++ src/main/java/com/web2/equipmentmaintenancecontrol/enums/ServiceRequestStatus.java
@@ -0,0 +1,28 @@
+package com.web2.equipmentmaintenancecontrol.enums;
+
+public enum ServiceRequestStatus {
+    ABERTA("Cinza", "#808080"),
+    ORCADA("Marrom", "#8B4513"),
+    REJEITADA("Vermelho", "#DC3545"),
+    APROVADA("Amarelo", "#FFC107"),
+    REDIRECIONADA("Roxo", "#6F42C1"),
+    ARRUMADA("Azul", "#007BFF"),
+    PAGA("Alaranjado", "#FD7E14"),
+    FINALIZADA("Verde", "#28A745");
+
+    private final String colorName;
+    private final String colorHex;
+
+    ServiceRequestStatus(String colorName, String colorHex){
+        this.colorName = colorName;
+        this.colorHex = colorHex;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public String getColorHex() {
+        return colorHex;
+    }
+}
