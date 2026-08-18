d4bb36021f01c41d6cee279865c41ee589d8d212
Joao Pivatto <jgpivatto@gmail.com>
2026-08-18 11:13:25 -0300

    Create project

---
+++ src/test/java/com/web2/equipmentmaintenancecontrol/EquipmentMaintenanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.equipmentmaintenancecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentMaintenanceControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
