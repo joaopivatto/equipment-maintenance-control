a6a2047aeea02cfd52342bf9d56c66dbe5a969b4
João Victor Dourado <joao@infoworker.com.br>
2026-08-18 20:06:22 -0300

    feat: add Equipment and EquipmentType models

---
+++ src/main/java/com/web2/equipmentmaintenancecontrol/model/Equipment.java
@@ -0,0 +1,39 @@
+package main.java.com.web2.equipmentmaintenancecontrol.model;
+
+public class Equipment {
+    private Integer id;
+    private String description;
+    private EquipmentType type;
+
+
+    public Equipment(Integer id, String description, EquipmentType type) {
+        this.id = id;
+        this.description = description;
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EquipmentType getType() {
+        return type;
+    }
+
+    public void setType(EquipmentType type) {
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/com/web2/equipmentmaintenancecontrol/model/EquipmentType.java
@@ -0,0 +1,29 @@
+package main.java.com.web2.equipmentmaintenancecontrol.model;
+
+public class EquipmentType {
+    private Integer id;
+    private String description;
+
+
+    public EquipmentType(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
