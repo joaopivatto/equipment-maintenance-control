f28d122b2d9aac0ad797244d96d0a46ddc6a1c19
JVDourado10 <jovneiva@gmail.com>
2026-09-05 19:49:48 -0300

    feat: new sidemenu and table style

--- equipment-maintenance-control-frontend/src/app/app.ts
+++ equipment-maintenance-control-frontend/src/app/app.ts
@@ -1,14 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Wrench } from '@primeicons/angular/wrench';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, SidebarModule, AvatarModule, ButtonModule, CardModule, Wrench],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('equipment-maintenance-control-frontend');
+
+  isMobile = signal(false);
+    constructor() {
+        if (typeof window === 'undefined') return;
+        const mql = window.matchMedia('(max-width: 1023px)');
+        this.isMobile.set(mql.matches);
+        mql.addEventListener('change', (e) => this.isMobile.set(e.matches));
+    }
 
   protected readonly navItems = [
     { label: 'Solicitações', icon: 'pi pi-list', link: '/requests/list' },
