4a3bc2fb3a9bc5937a4230fa1870ee77911962e3
Matheus Alves <matheusgalves.0604@gmail.com>
2026-09-24 19:47:59 -0300

    feat: use AppException when trowing an error in LoginService

--- equipment-maintenance-control-backend/src/main/java/com/web2/equipmentmaintenancecontrol/exception/ErrorCode.java
+++ equipment-maintenance-control-backend/src/main/java/com/web2/equipmentmaintenancecontrol/exception/ErrorCode.java
@@ -6,7 +6,8 @@ public enum ErrorCode {
   EQUIPMENT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "Equipment type not found"),
   EQUIPMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Equipment not found"),
   VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "Validation error"),
-  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected internal error");
+  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected internal error"),
+  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Invalid credentials");
 
   private final HttpStatus status;
   private final String message;
