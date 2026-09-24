fd9509035d40351513256fb10a0d0989d546c6c0
Matheus Alves <matheusgalves.0604@gmail.com>
2026-09-24 19:38:45 -0300

    style: fix style :)

--- equipment-maintenance-control-backend/src/main/java/com/web2/equipmentmaintenancecontrol/service/auth/LoginService.java
+++ equipment-maintenance-control-backend/src/main/java/com/web2/equipmentmaintenancecontrol/service/auth/LoginService.java
@@ -1,20 +1,13 @@
 package com.web2.equipmentmaintenancecontrol.service.auth;
 
-import com.web2.equipmentmaintenancecontrol.exception.AppException;
-import com.web2.equipmentmaintenancecontrol.exception.ErrorCode;
 import com.web2.equipmentmaintenancecontrol.model.auth.dtos.LoginRequest;
 import com.web2.equipmentmaintenancecontrol.model.auth.dtos.LoginResponse;
-import com.web2.equipmentmaintenancecontrol.model.equipment.Equipment;
-import com.web2.equipmentmaintenancecontrol.model.equipment.EquipmentType;
-import com.web2.equipmentmaintenancecontrol.model.equipment.dtos.EquipmentRequest;
-import com.web2.equipmentmaintenancecontrol.repository.equipment.EquipmentRepository;
-import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 
-    public LoginResponse execute(LoginRequest request) {
-        return new LoginResponse(true);
-    }
+  public LoginResponse execute(LoginRequest request) {
+    return new LoginResponse(true);
+  }
 }
